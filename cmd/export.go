062f31341456f053220108da95ee1c800bd79e79
Mike Rowehl <mikerowehl@gmail.com>
2026-01-08 14:45:08 -0800

    Remove the year from the copyright notice in the header.
    See the post about the curl project doing the same:
    https://daniel.haxx.se/blog/2023/01/08/copyright-without-years/

--- cmd/export.go
+++ cmd/export.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2025 - Mike Rowehl <mikerowehl@gmail.com>
+Copyright (c) Mike Rowehl <mikerowehl@gmail.com>
 This software may be modified and distributed under the terms of the MIT license.
 See LICENSE in the project root for full license information.
 */
